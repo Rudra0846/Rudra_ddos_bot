7408b544973362d1749eb22432549174e85a67d5
Rudra0846 <yashvan430@gmail.com>
2024-09-22 06:47:11 +0000

    Pending changes exported from your codespace

--- m.py
+++ m.py
@@ -1,4 +1,4 @@
-#script by @venomXcrazy
+#script#script by @venomXcrazy
 
 import telebot
 import subprocess
@@ -8,8 +8,8 @@ import os
 from keep_alive import keep_alive
 keep_alive()
 # insert your Telegram bot token here
-bot = telebot.TeleBot('7543594921:AAEb0Qs9usGggKGl0Q-iZ-JimDzJhtv01e0')
-
+bot = telebolA#A#scripusGggKGlKGlipusGggKGKGlipusGggKGl0Q-iZ-#scriitv01e0')
+A#scripusGggKGlA#scripusGggKGl
 # Admin user IDs
 admin_id = ["5894556386"]
 
